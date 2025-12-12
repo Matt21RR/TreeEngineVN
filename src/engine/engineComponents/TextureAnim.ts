468eb4d0fe81875cfdb373e8e38cb9f222da7bf6
Matt21RR <portal2matr@gmail.com>
2025-12-12 13:20:28 -0500

    minor changes

--- src/engine/engineComponents/TextureAnim.ts
+++ src/engine/engineComponents/TextureAnim.ts
@@ -3,7 +3,6 @@ class TextureAnim{
   private _list:Array<string> = [];
 
   private _duration:number = 1000;
-  private _timer:number = 0;
   private _speed:number = 1;
 
   constructor(tInfo){
@@ -14,7 +13,6 @@ class TextureAnim{
     this._list =      tInfo.list || [];
 
     this._duration =  tInfo.duration || 1000;
-    this._timer =     tInfo.timer || 0;
     this._speed =     tInfo.speed || 1;
 
   }
@@ -24,16 +22,12 @@ class TextureAnim{
   get duration() {return this._duration;}
   set duration(x) {this._duration = x;}
 
-  get timer() {return this._timer;}
-  set timer(x) {this._timer = x;}
-
   get speed() {return this._speed;}
   set speed(x) {this._speed = x;}
 
   getTexture(engDelta: number){
-    this._timer = engDelta;
     const listLength = this._list.length;
-    const delta = this._timer % this._duration;
+    const delta = engDelta % this._duration;
     const step = this._duration / listLength;
     const index = Math.floor(delta/step);
 
