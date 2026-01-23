2892c28a604eb9b6779001086ff61ea5092302b3
Matt21RR <portal2matr@gmail.com>
2026-01-22 23:58:09 -0500

    Update InputCheck.tsx

--- frontend/src/tools/components/inputs/InputCheck.tsx
+++ frontend/src/tools/components/inputs/InputCheck.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface InputCheckProps{
   id?: string,
@@ -8,29 +8,22 @@ interface InputCheckProps{
   selfSet?: (self: InputCheck)=>void
 }
 
-export default class InputCheck extends React.Component<InputCheckProps>{
-  id:string;
-  checked:boolean;
-  constructor(props){
-    super(props);
-    this.id = this.props.id ?? ("inputCheck" + String(window.performance.now()).replaceAll(".",""));
-    this.checked = this.props.checked ?? false;
+export default function InputCheck (props: InputCheckProps){
+  const [checked, setChecked] = useState(props.checked ?? false);
 
-    //SelfSet
-    this.props.selfSet?.(this);
-  }
-  render(){
-    return(
-      <div 
-        onClick={()=>{
-          this.checked = !this.checked;
-          this.forceUpdate();
-          this.props.action?.(this.checked)
-        }}
-        className="flex mx-3">
-        <div className={"border-[1px] mr-1 h-[0.8rem] w-[0.8rem] my-auto border-white "+(this.checked ? "bg-green-700": "")}/>
-        <span className="ml-1 my-auto">{this.props.label ?? "NOLABEL"}</span>
-      </div>
-    );
-  }
+  useEffect(()=>{
+    setChecked(props.checked);
+  },[props]);
+
+   return(
+    <div 
+      onClick={()=>{
+        setChecked(!checked);
+        props.action?.(checked);
+      }}
+      className="flex mx-3">
+      <div className={"border mr-1 h-[0.8rem] w-[0.8rem] my-auto border-white "+(checked ? "bg-green-700": "")}/>
+      <span className="ml-1 my-auto">{props.label ?? "NOLABEL"}</span>
+    </div>
+  );
 }
