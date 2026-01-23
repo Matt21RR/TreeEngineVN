a0086fb7f39ec104f3c1b5b9d918d912266608f8
Matt21RR <portal2matr@gmail.com>
2026-01-22 23:58:29 -0500

    Update EngineTools.tsx

--- frontend/src/tools/EngineTools.tsx
+++ frontend/src/tools/EngineTools.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { InputList} from "./components/InputList.tsx";
 import { Chaos } from "../engine/interpretators/ChaosInterpreter.ts";
 import { Dictionary } from "../global.ts";
@@ -12,145 +12,115 @@ interface EngineToolsProps {
   engine: RenderEngine
 }
 
-export default class EngineTools extends React.Component<EngineToolsProps>{
-  scripts:Dictionary;
-  sceneName:string;
-  selectedScript: number;
-  mounted:boolean;
-  constructor(props: EngineToolsProps){
-    super(props);
-    this.scripts = {};
-    this.sceneName = "gameEntrypoint";
-    this.selectedScript = 0;
-    this.mounted = false;
-  }
-  componentDidMount(){
-    if(!this.mounted){
-      this.mounted = true;
-      const chaos = new Chaos();
-      chaos.listScripts().then(scriptsId=>{
-        this.scripts = scriptsId;
-        this.forceUpdate();
-      })
-    }
+
+export default function EngineTools(props:EngineToolsProps){
+  const [scripts, setScripts] = useState<Dictionary<string>>({});
+  const [sceneName, setSceneName] = useState("gameEntrypoint");
+  const [selectedScript, setSelectedScript] = useState(0);
+
+  const [_, forceUpdate] = useReducer(x => x + 1, 0);
+
+  useEffect(()=>{
+    checkAvailableScripts();
+  },[]);
+
+  const checkAvailableScripts = ()=>{
+    const chaos = new Chaos();
+    chaos.listScripts().then(scriptsId=>{
+      setScripts(scriptsId);
+    });
   }
 
-  scriptSelector(){
-    const engine = this.props.engine;
+  const scriptSelector = () => {
+    const engine = props.engine;
     return (
       <div className="flex">
         <InputList 
-          options={Object.keys(this.scripts)}
-          action={(e)=>{this.selectedScript = e;}}
+          options={Object.keys(scripts)}
+          action={(e)=>{setSelectedScript(e);}}
           value={0}
         />
         <InputText style="bg-white text-black" defaultValue={"gameEntrypoint"} action={(value)=>{
-            this.sceneName = value != "" ? value : "gameEntrypoint";
+            setSceneName(value != "" ? value : "gameEntrypoint");
           }}/>
         <Button1 text={"Run!!"} action={()=>{
-          engine.loadScript(this.scripts[Object.keys(this.scripts)[this.selectedScript]], this.sceneName);
+          engine.loadScript(scripts[Object.keys(scripts)[selectedScript]], sceneName);
         }}/>
       </div>
     );
   }
-  speedControls(){
-    const engine = this.props.engine;
+  const speedControls = () => {
+    const engine = props.engine;
     return (
       <div className="flex flex-row text-white">
-        <IconButton icon="minus" action={()=>{engine.engineSpeed-= 0.1;this.forceUpdate();}}/>
+        <IconButton icon="minus" action={()=>{engine.engineSpeed-= 0.1; forceUpdate();}}/>
         <Button1 text={engine.stopEngine ? "Play" : "Stop"} action={()=>{
           engine.stopEngine = !engine.stopEngine;
-          this.forceUpdate();
+          forceUpdate();
         }}/>
-        <IconButton icon="plus" action={()=>{engine.engineSpeed+= 0.1;this.forceUpdate();}}/>
+        <IconButton icon="plus" action={()=>{engine.engineSpeed+= 0.1; forceUpdate();}}/>
         <span>Engine speed: {engine.engineSpeed.toFixed(2)}</span>
       </div>
     );
   }
-  cameraControls(){
-    
-    const engine = this.props.engine;
+  const cameraControls = () => {
+    const engine = props.engine;
     const camera = engine.camera;
+
     return (
       <div className="flex flex-col text-white">
-        {JSON.stringify(camera.position)}
-        <InputCheck label="Use perspective" checked={camera.usePerspective} action={(res:boolean)=>{camera.usePerspective = res}}/>
-        <div className="flex ">
-          X
-          <IconButton icon="minus" action={()=>{camera.position.x-= 0.1;this.forceUpdate();}}/>
-          <IconButton icon="plus" action={()=>{camera.position.x+= 0.1;this.forceUpdate();}}/>
+        <InputCheck 
+          label="Use perspective" 
+          checked={camera.usePerspective} 
+          action={(res:boolean)=>{camera.usePerspective = res; forceUpdate();}}/>
+        <div className="flex gap-2">
+          X:
+          <IconButton icon="minus" action={()=>{camera.position.x-= 0.1; forceUpdate();}}/>
+          <IconButton icon="plus" action={()=>{camera.position.x+= 0.1; forceUpdate();}}/>
+          {camera.position.x}
         </div>
-        <div className="flex ">
-          Y
-          <IconButton icon="minus" action={()=>{camera.position.y-= 0.1;this.forceUpdate();}}/>
-          <IconButton icon="plus" action={()=>{camera.position.y+= 0.1;this.forceUpdate();}}/>
+        <div className="flex gap-2">
+          Y:
+          <IconButton icon="minus" action={()=>{camera.position.y-= 0.1; forceUpdate();}}/>
+          <IconButton icon="plus" action={()=>{camera.position.y+= 0.1; forceUpdate();}}/>
+          {camera.position.y}
         </div>
-        <div className="flex ">
-          Z
-          <IconButton icon="minus" action={()=>{camera.position.z-= 0.1;this.forceUpdate();}}/>
-          <IconButton icon="plus" action={()=>{camera.position.z+= 0.1;this.forceUpdate();}}/>
+        <div className="flex gap-2">
+          Z:
+          <IconButton icon="minus" action={()=>{camera.position.z-= 0.1; forceUpdate();}}/>
+          <IconButton icon="plus" action={()=>{camera.position.z+= 0.1; forceUpdate();}}/>
+          {camera.position.z} 
         </div>
 
       </div>
     );
   }
-  fpsControls(){
-    const engine = this.props.engine;
-    if("object" in engine.canvasRef){
-      const canvasObject = engine.canvasRef.object;
-      const fps = canvasObject.targetFps;
-      return (
-        <div className="flex flex-row text-white">
-          <IconButton icon="minus" action={()=>{canvasObject.modFps(-1); this.forceUpdate();}}/>
-          <input 
-            className="bg-transparent" 
-            type="number" 
-            min={1} 
-            max={300} 
-            step={1} 
-            defaultValue={fps} 
-            onChange={(e)=>{
-              var value = parseFloat(e.target.value) || 24;
-              value = value > 0 ? value : 24;
-              canvasObject.setFps(value);
-              this.forceUpdate();
-            }}/>
-          <IconButton icon="plus" action={()=>{canvasObject.modFps(1); this.forceUpdate();}}/>
-          <span>Target FPS: {fps}</span>
-        </div>
-      );
-    }
-  }
 
-  buttons(){
-    const engine = this.props.engine;
+  const buttons = () => {
+    const engine = props.engine;
     const canvasObject = engine.canvasRef.object;
     return <div className="flex gap-6">
       <div>
         <Button1 text={`Perspective: ${engine.camera.usePerspective}`} action={()=>{
           engine.camera.usePerspective = !engine.camera.usePerspective;
-          this.forceUpdate();
         }}/>
         <Button1 text={"Restart canvas"} action={()=>{
           canvasObject.resetEngine();
-          this.forceUpdate();
         }}/>
         <Button1 text={"Draw triggers: "+engine.drawTriggers} action={()=>{
           engine.drawTriggers = !engine.drawTriggers;
           engine.forceUpdate();
-          this.forceUpdate();
         }}/>
       </div>
       <div>
         <Button1 text={"Draw collisions matrix: "+engine.drawCollisionsMatrix} action={()=>{
           engine.drawCollisionsMatrix = !engine.drawCollisionsMatrix;
           engine.forceUpdate();
-          this.forceUpdate();
         }}/>
         <Button1 text={"Show fps: "+engine.showFps} action={()=>{
           engine.showFps = !engine.showFps;
           canvasObject.showFps = !canvasObject.showFps;
-          this.forceUpdate();
+          forceUpdate();
           canvasObject.forceUpdate();
         }}/>
 
@@ -162,14 +132,13 @@ export default class EngineTools extends React.Component<EngineToolsProps>{
       </div>
   }
 
-  render(){
-    return(<div>
-      {this.buttons()}
 
-      {this.scriptSelector()}
-      {this.speedControls()}
-      {this.cameraControls()}
-      {this.fpsControls()}
-    </div>)
-  }
+  return(<div>
+    {buttons()}
+    <hr className="text-gray-200 my-1"/>
+    {scriptSelector()}
+    <hr className="text-gray-200 my-1"/>
+    {speedControls()}
+    {cameraControls()}
+  </div>)
 }
