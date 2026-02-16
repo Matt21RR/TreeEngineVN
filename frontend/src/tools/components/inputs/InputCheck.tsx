f3333040d3fb0873e63bc8e1497b825ea271f96c
Matt21RR <portal2matr@gmail.com>
2026-02-16 18:49:26 -0500

    Update InputCheck.tsx

--- frontend/src/tools/components/inputs/InputCheck.tsx
+++ frontend/src/tools/components/inputs/InputCheck.tsx
@@ -20,7 +20,7 @@ export default function InputCheck (props: InputCheckProps){
       onClick={()=>{
         props.action?.(!checked);
       }}
-      className="flex mx-3">
+      className="flex mx-2">
       <div className={"border mr-1 h-[0.8rem] w-[0.8rem] my-auto border-white "+(checked ? "bg-green-700": (props.uncheckedColor || ""))}/>
       <span className="ml-1 my-auto">{props.label ?? "NOLABEL"}</span>
     </div>
